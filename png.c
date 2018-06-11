587e62a0406de1dbb47c3f07358caf3720e0f36d
mathusummut <mathusum.mut@gmail.com>
2018-06-11 12:50:19 +0200

    Removed png warning

--- png.c
+++ png.c
@@ -2394,8 +2394,8 @@ png_compare_ICC_profile_with_sRGB(png_const_structrp png_ptr,
                       * discourage their use, skip the 'have_md5' warning below,
                       * which is made irrelevant by this error.
                       */
-                     png_chunk_report(png_ptr, "known incorrect sRGB profile",
-                         PNG_CHUNK_ERROR);
+                     /*png_chunk_report(png_ptr, "known incorrect sRGB profile",
+                         PNG_CHUNK_ERROR);*/
                   }
 
                   /* Warn that this being done; this isn't even an error since
